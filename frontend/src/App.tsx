b5af3efc85171c84961e34ff21061018a7924ed6
Nordin <75100826+Nordin-S@users.noreply.github.com>
2024-07-01 02:53:45 +0200

    Build error due to npm modules. reinstalled, works now.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect } from 'react';
 import axios from 'axios';
-import { verticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import logo from './logo.svg';
 import './App.css';
